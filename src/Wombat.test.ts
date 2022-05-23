5b1b13477817ea6778e244c3a16386f6589bf56c
Jan Schmutz <jan.schmutz@spielworks.com>
2022-05-23 16:47:12 +0200

    remove old tests

--- src/Wombat.test.ts
+++ src/Wombat.test.ts
@@ -1,8 +1,7 @@
-import { Chain, RpcEndpoint, UALErrorType } from 'universal-authenticator-library'
+import { Chain, RpcEndpoint } from 'universal-authenticator-library'
 import ScatterJS from '@scatterjs/core'
 import { Name } from './interfaces'
 import { Wombat } from './Wombat'
-import { UALWombatError } from './UALWombatError'
 
 declare var window: any
 
@@ -21,10 +20,10 @@ const chain: Chain = {
 
 const chains = [chain]
 
-const account: any = {
-  publicKey: 'EOS11111',
-  name: 'test.account',
-}
+// const account: any = {
+//   publicKey: 'EOS11111',
+//   name: 'test.account',
+// }
 
 // Make userAgent mutable for testing
 Object.defineProperty(window.navigator, 'userAgent', ((_value) => {
@@ -59,103 +58,12 @@ describe('Wombat', () => {
     })
   })
 
-  describe('logout', () => {
-    it('throws an error on failure', async () => {
-      const identity = { accounts: [account] }
-      scatter.getIdentity = jest.fn().mockImplementation(async () => identity)
-      scatter.logout = jest.fn().mockImplementation(() => { throw new Error('Error in logout') })
-      ScatterJS.scatter = scatter
-
-      const scatterAuth = new Wombat(chains, { appName: 'testdapp' })
-      await scatterAuth.init()
-
-      let didThrow = true
-
-      try {
-        await scatterAuth.logout()
-        didThrow = false
-      } catch (e) {
-        const ex = e as UALWombatError
-        expect(scatter.connect).toBeCalled()
-        expect(ex.source).toEqual(Name)
-        expect(ex.type).toEqual(UALErrorType.Logout)
-        expect(ex.cause).toEqual(new Error('Error in logout'))
-      }
-
-      expect(didThrow).toBe(true)
-    })
-  })
-
-  describe('login', () => {
-    it('should return a user per chain', async () => {
-      const identity = { accounts: [account] }
-      scatter.getIdentity = jest.fn().mockImplementation(async () => identity)
-      ScatterJS.scatter = scatter
-
-      const scatterAuth = new Wombat(chains, { appName: 'testdapp' })
-      await scatterAuth.init()
-
-      const users = await scatterAuth.login()
-      expect(users.length).toEqual(chains.length)
-      await expect(users[0].getAccountName()).resolves.toEqual(account.name)
-    })
-
-    it('clears user array on every login attempt', async () => {
-      const identity = { accounts: [account] }
-      scatter.getIdentity = jest.fn().mockImplementation(async () => identity)
-      ScatterJS.scatter = scatter
-
-      const scatterAuth = new Wombat(chains, { appName: 'testdapp' })
-      await scatterAuth.init()
-
-      await scatterAuth.login()
-      const users = await scatterAuth.login()
-      // Ensure we're not pushing duplicates
-      expect(users.length).toEqual(chains.length)
-    })
-
-    it('throws an error on failure', async () => {
-      scatter.getIdentity = jest.fn().mockImplementation(async () => { throw new Error('Error in getIdentity') })
-      ScatterJS.scatter = scatter
-
-      const scatterAuth = new Wombat([chain], { appName: 'testdapp' })
-      await scatterAuth.init()
-
-      let didThrow = true
-
-      try {
-        await scatterAuth.login()
-        didThrow = false
-      } catch (e) {
-        const ex = e as UALWombatError
-        expect(scatter.getIdentity).toBeCalled()
-        expect(ex.source).toEqual(Name)
-        expect(ex.type).toEqual(UALErrorType.Login)
-        expect(ex.cause!.message).toEqual('Unable load user\'s identity')
-      }
-
-      expect(didThrow).toBe(true)
-    })
-  })
-
   describe('isLoading', () => {
     it('is false when authenticator is not initialized', () => {
       ScatterJS.scatter = scatter
       const scatterAuth = new Wombat(chains, { appName: 'testdapp' })
 
       expect(scatterAuth.isLoading()).toBe(false)
-    })
-
-    it('is true while authenticator is initializing, and transitions when done', () => {
-      scatter.connect = () => {
-        return new Promise(() => { return })
-      }
-
-      ScatterJS.scatter = scatter
-      const scatterAuth = new Wombat([chain], { appName: 'testdapp' })
-      scatterAuth.init()
-
-      expect(scatterAuth.isLoading()).toBe(true)
     })
 
     it('is false when authenticator is initialized', async () => {
